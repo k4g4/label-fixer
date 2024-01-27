ee3b7c24794979168031b44056b8634b4c70f529
Andres Dejesus <andresdejesus123@gmail.com>
2024-01-27 11:43:50 -0600

    printer selection screen

--- src/bin/label-fixer-gui.rs
+++ src/bin/label-fixer-gui.rs
@@ -1,19 +1,20 @@
-#![cfg_attr(windows, windows_subsystem = "window")]
+#![cfg_attr(windows, windows_subsystem = "windows")]
 
 use iced::{
-    alignment, executor, mouse,
+    alignment, color, executor, mouse, theme,
     widget::{
         self,
         canvas::{self, Frame, Geometry, Program},
-        column, image, row, Canvas,
+        column, image, row, Button, Canvas,
     },
-    window, Application, Command, Element, Length, Rectangle, Renderer, Settings, Theme, Vector,
+    window, Application, Background, Color, Command, Element, Length, Rectangle, Renderer,
+    Settings, Theme, Vector,
 };
 use label_fixer::fix_label;
 
 const SIZE: (u32, u32) = (600, 600);
 const MIN_SIZE: (u32, u32) = (200, 400);
-const PRINTER_EMOJI: char = '🖶';
+const PRINTER_EMOJI: &str = "🖨️";
 const PRINTER_ICON: &[u8] = include_bytes!("../../printer.ico");
 
 fn main() -> iced::Result {
@@ -29,16 +30,32 @@ fn main() -> iced::Result {
     })
 }
 
+#[derive(Debug, Clone)]
+struct Printer(printers::printer::Printer);
+
+impl PartialEq for Printer {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.name == other.0.name
+    }
+}
+
+impl Eq for Printer {}
+
 #[derive(Default)]
 enum App {
     #[default]
     Empty,
     Loaded(image::Handle),
+    Printers(Vec<Printer>),
+    Printing(Printer),
 }
 
-#[derive(Debug, Clone, Hash)]
+#[derive(Debug, Clone)]
 enum Message {
     Load,
+    Print,
+    PrinterSelected(Printer),
+    Return,
 }
 
 impl Application for App {
@@ -80,41 +97,124 @@ impl Application for App {
                     _ => Command::none(),
                 }
             }
+
+            Message::Print => {
+                *self = Self::Printers(printers::get_printers().into_iter().map(Printer).collect());
+                Command::none()
+            }
+
+            Message::PrinterSelected(printer) => {
+                *self = Self::Printing(printer);
+                Command::none()
+            }
+
+            Message::Return => {
+                *self = Self::Empty;
+                Command::none()
+            }
         }
     }
 
     fn view(&self) -> Element<'_, Self::Message, Renderer<Self::Theme>> {
-        let open_button = widget::button("Open PDF")
-            .padding(10)
-            .on_press(Message::Load);
+        fn hot_pink() -> Color {
+            color!(0xf7007c)
+        }
 
-        let bottom_bar = row![
-            widget::horizontal_space(Length::Fill),
-            open_button,
-            widget::horizontal_space(Length::Fill)
-        ]
-        .padding(20);
+        struct PinkButtonTheme;
 
+        impl widget::button::StyleSheet for PinkButtonTheme {
+            type Style = Theme;
+
+            fn active(&self, style: &Self::Style) -> widget::button::Appearance {
+                widget::button::Appearance {
+                    border_radius: 15.0.into(),
+                    background: Some(Background::Color(hot_pink())),
+                    ..style.active(&theme::Button::Primary)
+                }
+            }
+        }
+
+        fn pink_button(title: &str, message: Message) -> Button<'_, Message> {
+            widget::button(title)
+                .style(theme::Button::custom(PinkButtonTheme))
+                .padding(10)
+                .on_press(message)
+        }
+
+        let open_button = pink_button("Open Label PDF", Message::Load);
+        let print_button = pink_button("Print Label", Message::Print);
         let sep = widget::container("")
-            .style(|theme: &Theme| widget::container::Appearance {
+            .style(|_: &Theme| widget::container::Appearance {
                 border_width: 2.0,
-                border_color: theme.palette().primary,
+                border_color: hot_pink(),
                 ..Default::default()
             })
             .width(Length::Fill)
             .max_height(2);
 
         match self {
-            Self::Empty => column![
-                Canvas::new(PrinterEmoji)
-                    .height(Length::Fill)
-                    .width(Length::Fill),
-                sep,
-                bottom_bar,
-            ],
-            Self::Loaded(handle) => column![image::viewer(handle.clone()), sep, bottom_bar],
+            Self::Empty => {
+                let bottom_bar = row![
+                    widget::horizontal_space(Length::Fill),
+                    open_button,
+                    widget::horizontal_space(Length::Fill)
+                ]
+                .padding(20);
+
+                column![
+                    Canvas::new(PrinterEmoji)
+                        .height(Length::Fill)
+                        .width(Length::Fill),
+                    sep,
+                    bottom_bar,
+                ]
+                .into()
+            }
+
+            Self::Loaded(handle) => {
+                let bottom_bar = row![
+                    widget::horizontal_space(Length::Fill),
+                    open_button,
+                    widget::horizontal_space(20),
+                    print_button,
+                    widget::horizontal_space(Length::Fill)
+                ]
+                .padding(20);
+
+                column![
+                    image::viewer(handle.clone())
+                        .height(Length::Fill)
+                        .width(Length::Fill),
+                    sep,
+                    bottom_bar
+                ]
+                .into()
+            }
+
+            Self::Printers(printers) => {
+                let buttons = {
+                    let mut buttons = vec![];
+                    for printer in printers {
+                        buttons.push(
+                            pink_button(
+                                printer.0.name.as_str(),
+                                Message::PrinterSelected(printer.clone()),
+                            )
+                            .into(),
+                        );
+                        buttons.push(widget::vertical_space(30).into());
+                    }
+                    buttons.push(pink_button("Return", Message::Return).into());
+                    buttons
+                };
+
+                column(buttons).into()
+            }
+
+            Self::Printing(printer) => {
+                widget::text(format!("Printing to {}...", printer.0.name)).into()
+            }
         }
-        .into()
     }
 
     fn theme(&self) -> Self::Theme {
