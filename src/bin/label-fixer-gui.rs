4eeaf5f95c12f24301664cc7d290711d655894ec
Andres Dejesus <andresdejesus123@gmail.com>
2024-01-27 17:32:16 -0600

    doesn't seem to print correctly

--- src/bin/label-fixer-gui.rs
+++ src/bin/label-fixer-gui.rs
@@ -1,20 +1,25 @@
 #![cfg_attr(windows, windows_subsystem = "windows")]
 
+use std::path::PathBuf;
+
 use iced::{
-    alignment, color, executor, mouse, theme,
+    alignment::{self, Horizontal},
+    color, executor, mouse,
+    theme::{self, Palette},
     widget::{
         self,
         canvas::{self, Frame, Geometry, Program},
-        column, image, row, Button, Canvas,
+        column, horizontal_space, image, row, text, vertical_space, Button, Canvas,
     },
-    window, Application, Background, Color, Command, Element, Length, Rectangle, Renderer,
-    Settings, Theme, Vector,
+    window, Alignment, Application, Background, Color, Command, Element, Length, Rectangle,
+    Renderer, Settings, Theme, Vector,
 };
 use label_fixer::fix_label;
 
 const SIZE: (u32, u32) = (600, 600);
 const MIN_SIZE: (u32, u32) = (200, 400);
 const PRINTER_EMOJI: &str = "🖨️";
+const SUCCESS_EMOJI: &str = "✅";
 const PRINTER_ICON: &[u8] = include_bytes!("../../printer.ico");
 
 fn main() -> iced::Result {
@@ -41,21 +46,35 @@ impl PartialEq for Printer {
 
 impl Eq for Printer {}
 
+#[derive(Debug, Clone)]
+enum Message {
+    Load,
+    Print(PathBuf),
+    PrinterSelected(Printer, PathBuf),
+    Return(PathBuf),
+}
+
+fn error_dialog(text: &str) {
+    native_dialog::MessageDialog::new()
+        .set_title("Error")
+        .set_text(text)
+        .show_alert()
+        .unwrap()
+}
+
 #[derive(Default)]
 enum App {
     #[default]
     Empty,
-    Loaded(image::Handle),
-    Printers(Vec<Printer>),
-    Printing(Printer),
+    Loaded(image::Handle, PathBuf),
+    Printers(Vec<Printer>, PathBuf),
+    Success(Printer),
 }
 
-#[derive(Debug, Clone)]
-enum Message {
-    Load,
-    Print,
-    PrinterSelected(Printer),
-    Return,
+impl App {
+    fn loaded(out_path: PathBuf) -> Self {
+        Self::Loaded(image::Handle::from_path(&out_path), out_path)
+    }
 }
 
 impl Application for App {
@@ -69,7 +88,7 @@ impl Application for App {
     }
 
     fn title(&self) -> String {
-        "Label Fixer".into()
+        "Depop Label Fixer".into()
     }
 
     fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
@@ -82,15 +101,9 @@ impl Application for App {
                     Ok(Some(path)) => {
                         match fix_label(path) {
                             Ok(out_path) => {
-                                *self = Self::Loaded(image::Handle::from_path(out_path));
+                                *self = Self::loaded(out_path);
                             }
-                            Err(error) => {
-                                native_dialog::MessageDialog::new()
-                                    .set_title("Error")
-                                    .set_text(&format!("{error}"))
-                                    .show_alert()
-                                    .unwrap();
-                            }
+                            Err(error) => error_dialog(&error.to_string()),
                         }
                         Command::none()
                     }
@@ -98,18 +111,35 @@ impl Application for App {
                 }
             }
 
-            Message::Print => {
-                *self = Self::Printers(printers::get_printers().into_iter().map(Printer).collect());
+            Message::Print(out_path) => {
+                *self = Self::Printers(
+                    printers::get_printers().into_iter().map(Printer).collect(),
+                    out_path,
+                );
                 Command::none()
             }
 
-            Message::PrinterSelected(printer) => {
-                *self = Self::Printing(printer);
+            Message::PrinterSelected(printer, out_path) => {
+                println!("{}", out_path.to_str().unwrap());
+                *self = match printer.0.print(&std::fs::read(&out_path).unwrap(), None) {
+                    Ok(true) => Self::Success(printer),
+                    Ok(false) => {
+                        error_dialog(&format!("{} returned 'false'", printer.0.name));
+                        Self::loaded(out_path)
+                    }
+                    Err(error) => {
+                        error_dialog(&format!(
+                            "Error while printing to {}: '{error}'",
+                            printer.0.name
+                        ));
+                        Self::loaded(out_path)
+                    }
+                };
                 Command::none()
             }
 
-            Message::Return => {
-                *self = Self::Empty;
+            Message::Return(out_path) => {
+                *self = Self::loaded(out_path);
                 Command::none()
             }
         }
@@ -142,7 +172,6 @@ impl Application for App {
         }
 
         let open_button = pink_button("Open Label PDF", Message::Load);
-        let print_button = pink_button("Print Label", Message::Print);
         let sep = widget::container("")
             .style(|_: &Theme| widget::container::Appearance {
                 border_width: 2.0,
@@ -155,14 +184,14 @@ impl Application for App {
         match self {
             Self::Empty => {
                 let bottom_bar = row![
-                    widget::horizontal_space(Length::Fill),
+                    horizontal_space(Length::Fill),
                     open_button,
-                    widget::horizontal_space(Length::Fill)
+                    horizontal_space(Length::Fill)
                 ]
                 .padding(20);
 
                 column![
-                    Canvas::new(PrinterEmoji)
+                    Canvas::new(EmojiDisplayer(PRINTER_EMOJI))
                         .height(Length::Fill)
                         .width(Length::Fill),
                     sep,
@@ -171,13 +200,13 @@ impl Application for App {
                 .into()
             }
 
-            Self::Loaded(handle) => {
+            Self::Loaded(handle, out_path) => {
                 let bottom_bar = row![
-                    widget::horizontal_space(Length::Fill),
+                    horizontal_space(Length::Fill),
                     open_button,
-                    widget::horizontal_space(20),
-                    print_button,
-                    widget::horizontal_space(Length::Fill)
+                    horizontal_space(20),
+                    pink_button("Print Label", Message::Print(out_path.clone())),
+                    horizontal_space(Length::Fill)
                 ]
                 .padding(20);
 
@@ -191,40 +220,74 @@ impl Application for App {
                 .into()
             }
 
-            Self::Printers(printers) => {
+            Self::Printers(printers, out_path) => {
                 let buttons = {
-                    let mut buttons = vec![];
+                    let mut buttons = vec![vertical_space(30).into()];
                     for printer in printers {
                         buttons.push(
                             pink_button(
                                 printer.0.name.as_str(),
-                                Message::PrinterSelected(printer.clone()),
+                                Message::PrinterSelected(printer.clone(), out_path.clone()),
                             )
+                            .width(Length::Fill)
                             .into(),
                         );
-                        buttons.push(widget::vertical_space(30).into());
+                        buttons.push(vertical_space(30).into());
                     }
-                    buttons.push(pink_button("Return", Message::Return).into());
+                    buttons.push(pink_button("Return", Message::Return(out_path.clone())).into());
                     buttons
                 };
 
-                column(buttons).into()
+                row![
+                    horizontal_space(Length::FillPortion(1)),
+                    column(buttons)
+                        .align_items(Alignment::Center)
+                        .width(Length::FillPortion(2)),
+                    horizontal_space(Length::FillPortion(1)),
+                ]
+                .into()
             }
 
-            Self::Printing(printer) => {
-                widget::text(format!("Printing to {}...", printer.0.name)).into()
+            Self::Success(printer) => {
+                let top_message = row![
+                    horizontal_space(Length::Fill),
+                    text(format!("Printed to {} successfully!", printer.0.name))
+                        .horizontal_alignment(Horizontal::Center)
+                        .size(30),
+                    horizontal_space(Length::Fill),
+                ];
+
+                let bottom_bar = row![
+                    horizontal_space(Length::Fill),
+                    open_button,
+                    horizontal_space(Length::Fill)
+                ]
+                .padding(20);
+
+                column![
+                    top_message,
+                    Canvas::new(EmojiDisplayer(SUCCESS_EMOJI))
+                        .height(Length::Fill)
+                        .width(Length::Fill),
+                    sep,
+                    bottom_bar,
+                ]
+                .into()
             }
         }
     }
 
     fn theme(&self) -> Self::Theme {
-        Theme::Dark
+        Theme::custom(Palette {
+            background: color!(0x2f2536),
+            ..Theme::Dark.palette()
+        })
     }
 }
 
-struct PrinterEmoji;
+struct EmojiDisplayer(&'static str);
 
-impl Program<Message> for PrinterEmoji {
+impl Program<Message> for EmojiDisplayer {
     type State = ();
 
     fn draw(
@@ -238,7 +301,7 @@ impl Program<Message> for PrinterEmoji {
         let mut frame = Frame::new(renderer, bounds.size());
         frame.translate(Vector::new(bounds.width * 0.5, bounds.height * 0.25));
         frame.fill_text(canvas::Text {
-            content: PRINTER_EMOJI.to_string(),
+            content: self.0.to_string(),
             shaping: widget::text::Shaping::Advanced,
             size: 100.0 + bounds.height * 0.3,
             horizontal_alignment: alignment::Horizontal::Center,
